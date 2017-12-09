be4e8b505a1e3198ea116301e939814947bb8bab
Scayze <manuel.strey@gmx.de>
2017-12-09 16:06:42 +0100

    Removed unnecessary level selector image thingy

--- Menu/HSplitContainer.gd
+++ Menu/HSplitContainer.gd
@@ -1,13 +1,9 @@
 extends Control
 
-onready var select_frame = get_parent().get_node("TextureButton")
 var selected_level = null
 
 func _ready():
-	#dragger_visibility = DRAGGER_VISIBLE
 	pass
 
 func select_level(level):
 	selected_level = level
-	select_frame.rect_position = level.rect_position
-	pass
